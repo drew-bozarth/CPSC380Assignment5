acff9629fee2d159d5c25d67f5b86fd15419329c
Romtin07 <romtin@pickpillar.com>
2022-10-29 21:39:59 -0700

    initial commit

--- prodcon.c
+++ prodcon.c
@@ -1,83 +1,218 @@
+#include <time.h>
+#include <stdint.h>
+#include <sys/types.h>
 #include <pthread.h>
 #include <semaphore.h>
-#include <stdlib.h>
+#include <errno.h>
+#include <sys/shm.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+
+#include <time.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
 
-/*
-This program provides a possible solution for producer-consumer problem using mutex and semaphore.
-I have used 5 producers and 5 consumers to demonstrate the solution. You can always play with these values.
-*/
+typedef struct item {
+  unsigned int seqn; 
+  uint8_t data[22];  
+  time_t timestamp;  
+  uint16_t checksum; 
+} ITEM;  
 
-#define MaxItems 5 // Maximum items a producer can produce or a consumer can consume
-#define BufferSize 5 // Size of the buffer
+uint16_t checksum(char *address, uint32_t tally)
+{
+    register uint32_t sum = 0;
 
-sem_t empty;
-sem_t full;
-int in = 0;
-int out = 0;
-int buffer[BufferSize];
+    uint16_t *buf = (uint16_t *) address;
+
+    // Main summing loop
+    while(tally > 1)
+    {
+        sum = sum + *(buf)++;
+        tally = tally - 2;
+    }
+
+    // Add left-over byte, if any
+    if (tally > 0)
+        sum = sum + *address;
+
+    // Fold 32-bit sum to 16 bits
+    while (sum>>16)
+        sum = (sum & 0xFFFF) + (sum >> 16);
+
+    return(~sum);
+
+}
+
+sem_t *empty;
+sem_t *full;
 pthread_mutex_t mutex;
 
+char* shm_name = "shm";
+struct stat buf;
+uint8_t  *shm_ptr;
+int shm_fd;
 
-// Producer code
-void *producer(void *pno)
-{   
-    int item;
-    for(int i = 0; i < MaxItems; i++) {
-        item = rand(); // Produce an random item
-        sem_wait(&empty);
-        pthread_mutex_lock(&mutex);
-        buffer[in] = item;
-        printf("Producer %d: Insert Item %d at %d\n", *((int *)pno),buffer[in],in);
-        in = (in+1)%BufferSize;
-        pthread_mutex_unlock(&mutex);
-        sem_post(&full);
-    }
+int in = 0;
+int out = 0;
+
+void* producer();
+void* consumer();
+
+int n_items;
+
+int main(int argc, char *argv[]){
+
+  // check for mis-input
+  if (argc != 2){
+    printf("Incorrect args");
+    return -1;
+  }
+
+  /* declare size */
+  n_items = atoi(argv[1]);
+
+  sem_unlink("empty");
+  sem_unlink("full");
+  shm_unlink(shm_name);
+
+  /* create and initialize the mutex and semaphores */
+  pthread_mutex_init(&mutex, NULL);
+  empty = sem_open("/empty", O_CREAT, 0644, n_items);
+  full = sem_open("/full", O_CREAT, 0644, 0);
+
+  /* create the shared memory segment */
+  shm_fd = shm_open(shm_name, O_CREAT | O_RDWR, 0644);
+  if (shm_fd == -1) {
+      fprintf(stderr, "Error unable to create shared memory, '%s, errno = %d (%s)\n", shm_name,
+        errno, strerror(errno));
+      return -1;
+  }
+
+  /* configure the size of the shared memory segment */
+  if (ftruncate(shm_fd, n_items*sizeof(ITEM)) == -1) {
+      fprintf(stderr, "Error configure create shared memory, '%s, errno = %d (%s)\n", shm_name,
+        errno, strerror(errno));
+      shm_unlink(shm_name);
+      return -1;
+  }
+
+  /* get configuration of shared memory segment */
+  if (fstat(shm_fd, &buf) == -1) {
+      fprintf(stderr, "Error unable to status shared memory segment fd = %d, errno = %d (%s)\n", shm_fd,
+              errno, strerror(errno));
+      return -1;
+  }
+
+  /* attach to shared memory region */
+  shm_ptr = (uint8_t *)mmap(0, buf.st_size, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+  if (shm_ptr == MAP_FAILED) {
+      fprintf(stderr, "Error: unable to map shared memory segment, errno = %d (%s) \n",
+              errno, strerror(errno));
+      return -1;
+  }
+
+  pthread_t a_thread[2];
+  void *thread_result;
+  int res;
+
+  res = pthread_create(&a_thread[0], NULL, producer, NULL);
+  if (res != 0) {
+      perror("Producer thread creation failed");
+      exit(EXIT_FAILURE);
+  }
+
+  res = pthread_create(&a_thread[1], NULL, consumer, NULL);
+  if (res != 0) {
+      perror("Consumer thread creation failed");
+      exit(EXIT_FAILURE);
+  }
+
+  pthread_join(a_thread[0],NULL);
+  pthread_join(a_thread[1],NULL);
+
+  /* remove the shared memory segment */
+  if (shm_unlink(shm_name) == -1) {
+      fprintf(stderr, "Error unable to remove shared memory segment '%s', errno = %d (%s) \n", shm_name,
+              errno, strerror(errno));
+      return -1;
+  }
+
+  // sem_unlink("full");
+  // sem_unlink("empty");
+
 }
 
-// Consumer code 
-void *consumer(void *cno)
-{   
-    for(int i = 0; i < MaxItems; i++) {
-        sem_wait(&full);
-        pthread_mutex_lock(&mutex);
-        int item = buffer[out];
-        printf("Consumer %d: Remove Item %d from %d\n",*((int *)cno),item, out);
-        out = (out+1)%BufferSize;
-        pthread_mutex_unlock(&mutex);
-        sem_post(&empty);
+// thread functions
+void* producer(void* arg){
+
+  ITEM item;
+  item.data[0] = (uint8_t)rand() % 256;
+  item.checksum = checksum((char*) item.data, 22);
+
+  srand (time(0));
+  int counter = 0;
+  while(counter<n_items){
+    item.seqn = counter++;
+    item.timestamp = time(NULL);
+    for(int j = 0; j <22; ++j){
+      item.data[j] = (uint8_t)rand() % 256;
     }
+    item.checksum = checksum((char*) item.data, 22);
+
+    printf("sum: %s\n", item.data);
+    fflush(stdout);
+
+    sem_wait(empty);
+    pthread_mutex_lock(&mutex);
+      //critical section
+
+      memcpy((void*) &shm_ptr[in], (void*) &item, sizeof(ITEM));
+      in = (in + 1) % n_items;
+
+    pthread_mutex_unlock(&mutex);
+    sem_post(full);
+    printf("Producer item %s\n", item.data);
+    fflush(stdout);
+  }
+  return arg;
 }
 
-// Main code 
-int main()
-{   
+void *consumer(void* arg){
 
-    pthread_t pro[5],con[5];
-    pthread_mutex_init(&mutex, NULL);
-    sem_init(&empty,0,BufferSize);
-    sem_init(&full,0,0);
+  ITEM item;
+  unsigned int seqn;
 
-    int a[5] = {1,2,3,4,5}; //Just used for numbering the producer and consumer
+  while(1) {
+    sem_wait(full);
+    pthread_mutex_lock(&mutex);
+      //critical section
+      memcpy((void*) &item, (void*) &shm_ptr[out], sizeof(ITEM));
+      out = (out + 1) % n_items;
 
-    for(int i = 0; i < 5; i++) {
-        pthread_create(&pro[i], NULL, (void *)producer, (void *)&a[i]);
+    pthread_mutex_unlock(&mutex);
+    sem_post(empty);
+
+    //check sequenceNum
+    seqn = item.seqn;
+
+    // if (///////) {
+    //   perror("failed");
+    //   exit(EXIT_FAILURE);
+    // }
+
+    uint16_t cksum = checksum((char*) item.data, 22);
+    if (item.checksum != cksum) {
+        printf("failed %u %s\n", cksum, item.data);
+        fflush(stdout);
+        // exit(EXIT_FAILURE);
     }
-    for(int i = 0; i < 5; i++) {
-        pthread_create(&con[i], NULL, (void *)consumer, (void *)&a[i]);
-    }
 
-    for(int i = 0; i < 5; i++) {
-        pthread_join(pro[i], NULL);
-    }
-    for(int i = 0; i < 5; i++) {
-        pthread_join(con[i], NULL);
-    }
+  }
 
-    pthread_mutex_destroy(&mutex);
-    sem_destroy(&empty);
-    sem_destroy(&full);
 
-    return 0;
-    
+  return arg;
 }
